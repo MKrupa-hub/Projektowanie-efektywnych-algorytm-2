3e746e67b753133caf9a87fcda14cfcd782b6a3b
MKrupa-hub <mateuszkrupa123@wp.pl>
2022-01-12 08:31:18 +0100

    Done

---
+++ PEA2/Program.cs
@@ -0,0 +1,163 @@
+﻿
+using System;
+using System.IO;
+
+namespace PEA2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int timeStop = 1000;
+            int[,] numArray = new int[,] { };
+            int city = 0;
+            string choice = "";
+            string filename = "UWAGA BRAK PLIKU ! ";
+            do
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("1. Wybierz macierz z pliku" + "                             ||aktualny plik: " + filename+"||");
+                Console.WriteLine("2. Wprowadzenie kryterium stopu" + "                        ||aktualny próg stopu: " + timeStop / 1000 + "[s]" + "/" + timeStop + "[ms]||");
+                Console.WriteLine("3. Uruchomienie algorytmu oraz wyświetlenie wyników");
+                Console.WriteLine("4. Wyjście");
+                Console.Write("\r\nWybor: ");
+                choice = Console.ReadLine();
+                
+                switch (choice)
+                {
+                    case "1":
+
+                        Console.Clear();
+                        bool fileFound = false;
+                        do
+                        {
+                            try
+                            {
+                                Console.WriteLine("---------------------------");
+                                Console.WriteLine("\r\nPodaj nazwe pliku");
+                                Console.Write("\r\nNazwa: ");
+                                filename = Console.ReadLine();
+                                using (StreamReader file = new StreamReader(filename + ".txt"))
+                                {
+
+                                    string ln;
+                                    city = Int32.Parse(file.ReadLine());
+                                    int ft = 0; int st = 0;
+                                    numArray = new int[city, city];
+                                    while ((ln = file.ReadLine()) != null)
+                                    {
+                                        char[] separators = new char[] { ' ', '.' };
+                                        string[] subs = ln.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                                        foreach (var single_number in subs)
+                                        {
+
+                                            numArray[ft, st] = Int32.Parse(single_number);
+                                            st++;
+                                        }
+                                       
+                                            st = 0;
+                                            ft++;
+                                        
+                                        
+                                    }
+                                    //dla miast tych samych wpisujemy droge -1
+                                    for (int i = 0; i < city; i++)
+                                    {
+                                        numArray[i, i] = -1;
+                                    }
+                                    file.Close();
+                                    fileFound = true;
+                                }
+                            }
+                            catch (FileNotFoundException e) { Console.Clear(); Console.Write("Plik nie istnieje\n"); }
+                        }
+                        while (!fileFound);
+                        break;
+                    case "2":
+                        int s_choice;
+                        Console.Clear();
+                        Console.WriteLine("---------------------------");
+                        Console.WriteLine("1. Podaj czas w [s]");
+                        Console.WriteLine("2. Podaj czas w [ms]");
+                        Console.Write("\r\nWybor: ");
+                        s_choice = Int32.Parse(Console.ReadLine());
+                        if (s_choice == 1)
+                        {
+                            Console.WriteLine("\r\nPodaj czas w [s]");
+                            timeStop = Int32.Parse(Console.ReadLine()) * 1000;
+                            
+                        }
+                        else if (s_choice == 2)
+                        {
+                            Console.WriteLine("\r\nPodaj czas w [ms]");
+                            timeStop = Int32.Parse(Console.ReadLine());
+                        }
+   
+                        break;
+                        
+
+                    case "3":
+                        for (int i = 0; i < 5; i++)
+                        {
+                            SimulatedAnnealing SA = new SimulatedAnnealing(timeStop, numArray, city);
+                            SA.Loop();
+                        }
+                        break;
+
+                    case "10":
+                        randommatrix(55);
+                        break;
+                    default:
+
+                        break;
+
+
+
+                }
+            } while (choice != "4");
+
+        }
+
+
+
+
+        static void randommatrix(int city)
+        {
+            int[,] numArray = new int[,] { };
+            numArray = new int[city, city];
+            // przypisujemy losowe wartosci dla macierzy
+            Random rnd = new Random();
+            for (int i = 0; i < numArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < numArray.GetLength(1); j++)
+                {
+                    numArray[i, j] = rnd.Next(1, 88);
+                    
+                }
+                
+            }
+            //dla miast tych samych wpisujemy droge -1
+            for (int i = 0; i < city; i++)
+            {
+                numArray[i, i] = -1;
+            }
+            for (int i = 0; i < numArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < numArray.GetLength(1); j++)
+                {
+                    
+                    Console.Write(numArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+
+    }
+
+
+
+}
---
+++ PEA2/SimulatedAnnealing.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEA2
+{
+    class SimulatedAnnealing
+    {
+        private int timeStop;
+        private int[,] numArray = new int[,] { };
+        private int[] currentPath = new int[] { };
+        private int[] possiblePath = new int[] { };
+        private int[] bestPath = new int[] { };
+        private int city = 0;
+        private double T ;
+        private double factor = 0.99;
+        private int when_decrease = 200;
+        private Random rnd = new Random();
+        private int samePath = 0;
+        public SimulatedAnnealing(int timeStop, int[,] numArray, int city)
+        {
+            this.timeStop = timeStop;
+            this.numArray = (int[,])numArray.Clone();
+            this.city = city;
+            this.T = city * city;
+        }
+
+
+        public void GeneratePath()
+        {
+            //generowanie poczatkowej sciezki
+            currentPath = new int[city + 1];
+            for (int i = 0; i < currentPath.Length - 1; i++)
+            {
+                currentPath[i] = i;
+            }
+            int j;
+            for (int i = 1; i < currentPath.Length - 1; i++)
+            {
+                j = rnd.Next(1, currentPath.Length - 1);
+                (currentPath[i], currentPath[j]) = (currentPath[j], currentPath[i]);
+            }
+            currentPath[city] = 0;
+        }
+
+        //zwrocenie dlugosci drogi
+        public int GetDisance(int[] table)
+        {
+            int dist = 0;
+            for (int i = 0; i < table.Length - 1; i++)
+            {
+                dist += numArray[table[i], table[i + 1]];
+            }
+
+
+            return dist;
+        }
+
+        //funkcja ktora zamienia miejscami dwa losowe miasta
+        public void Swap()
+        {
+
+            int firstIndex = rnd.Next(1, city);
+            int secondIndex;
+            do
+            {
+                secondIndex = rnd.Next(1, city);
+            } while (firstIndex == secondIndex);
+
+            (possiblePath[firstIndex], possiblePath[secondIndex]) = (possiblePath[secondIndex], possiblePath[firstIndex]);
+
+        }
+
+        //zamiana aktualnej sciezki 
+        public void SwapPath()
+        {
+            double b = rnd.NextDouble();
+            double a = Math.Exp(-(GetDisance(possiblePath) - GetDisance(currentPath)) / T);
+            
+            if (GetDisance(possiblePath) <= GetDisance(currentPath))
+            {
+                currentPath = (int[])possiblePath.Clone();
+                bestPath = (int[])possiblePath.Clone();
+                samePath = 0;
+               
+            }
+            else if (a  > b)
+            {
+                
+                currentPath = (int[])possiblePath.Clone();
+                samePath = 0;
+                
+            }
+            else
+            {
+                samePath++;
+               
+            }
+
+
+
+        }
+        //glowna petla algorytmu
+        public void Loop()
+        {
+            GeneratePath();
+            int ready = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            do
+            {
+                stopWatch.Start();
+                ready++;
+                if (ready == when_decrease) {
+                    T = T * factor;
+                    ready = 0;
+                }
+                
+                possiblePath = (int[])currentPath.Clone();
+                
+                Swap();
+                SwapPath();            
+                if (samePath == 10000000) { break; }
+                stopWatch.Stop();
+            }
+            while (stopWatch.Elapsed.TotalMilliseconds < timeStop);
+            Console.WriteLine("Czas: " + Math.Floor(stopWatch.Elapsed.TotalMilliseconds / 1000) +"[s]" + "/" + Math.Round(stopWatch.Elapsed.TotalMilliseconds) + "[ms]");
+            
+            
+            Console.WriteLine("Dystans: " + GetDisance(bestPath));
+            //for (int i = 0; i < bestPath.Length - 1; i++)
+            //{
+            //    Console.Write(bestPath[i] + "-");
+            //}
+            //Console.Write("0");
+            Console.WriteLine();
+
+
+
+        }
+
+
+
+    }
+}
